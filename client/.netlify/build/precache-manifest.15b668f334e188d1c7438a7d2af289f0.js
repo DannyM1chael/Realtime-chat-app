85cfa701ccf2b5ac8e7d7e7f96f51182b6d5cba3
DannyM1chael <coldsoulrusirk@gmail.com>
2020-02-23 14:13:20 +0300

    23/02/2020

---
+++ client/.netlify/build/precache-manifest.15b668f334e188d1c7438a7d2af289f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e757eb8e1f0253937560be516685be7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91cb853fb6ecab2f6467",
+    "url": "/static/css/main.c220a443.chunk.css"
+  },
+  {
+    "revision": "f47c3be4c1dd18090874",
+    "url": "/static/js/2.992661bc.chunk.js"
+  },
+  {
+    "revision": "8345da3dc262b15c21e065faca7dd545",
+    "url": "/static/js/2.992661bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91cb853fb6ecab2f6467",
+    "url": "/static/js/main.ecb72e15.chunk.js"
+  },
+  {
+    "revision": "2d5f851286933f250399",
+    "url": "/static/js/runtime-main.b25b76e5.js"
+  }
+]);
